82d15f6d39a2ab264b187b28d66414c895e15cf4
stursyn <stursyn@greet-go.com>
2020-07-05 16:57:17 +0600

    add yandex map

--- carpet_cleaner.mobile/src/app/tab1/tab1.module.ts
+++ carpet_cleaner.mobile/src/app/tab1/tab1.module.ts
@@ -1,11 +1,12 @@
 import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import {MapModule} from "../map/map.module";
 
 @NgModule({
   imports: [
@@ -13,8 +14,10 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    MapModule,
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Tab1PageModule {}
